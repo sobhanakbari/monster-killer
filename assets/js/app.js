91d29828fe0260dbd8a9dac4f3e3b71bfb2275ed
unknown <sobhanpick@gmail.com>
2024-02-03 20:23:01 +0330

    add if-else

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,15 @@ adjustHealthBars(chosenMaxlife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
-  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth >= 0) {
+    alertI("you win!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth >= 0) {
+    alert("you lost!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("you have drow!");
+  }
 }
 
 attackBtn.addEventListener("click", attackHandler);
