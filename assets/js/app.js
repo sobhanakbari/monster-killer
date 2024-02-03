646b18d3d006cc5b5f8ece2acf10385766dc08fa
unknown <sobhanpick@gmail.com>
2024-02-03 19:47:29 +0330

    add function

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,11 @@ let chosenMaxlife = 100;
 let currentMonsterHealth = chosenMaxlife;
 let currentPlayerHealth = chosenMaxlife;
 
+adjustHealthBars(chosenMaxlife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+}
+
+attackBtn.addEventListener("click", attackHandler);
